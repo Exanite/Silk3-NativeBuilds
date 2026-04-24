7934187c3a417bd868a1305b5fba826d3e3108bb
William Chen <exanite78012@gmail.com>
2026-04-23 21:53:11 -0400

    Also build VMA

---
+++ sources/Vulkan/Vma.Native/src/Vma.cpp
@@ -0,0 +1,15 @@
+#if defined _WIN32
+#define VMA_CALL_PRE __declspec(dllexport)
+#else
+#define VMA_CALL_PRE __attribute__((visibility("default")))
+#endif
+
+#define VMA_IMPLEMENTATION
+
+// Disable static linking
+// Enable dynamic function pointer fetching
+// User must provide pointers for vkGetInstanceProcAddr and vkGetDeviceProcAddr
+#define VMA_STATIC_VULKAN_FUNCTIONS 0
+#define VMA_DYNAMIC_VULKAN_FUNCTIONS 1
+
+#include <vk_mem_alloc.h>
